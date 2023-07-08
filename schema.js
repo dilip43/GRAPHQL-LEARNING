eac3e70a5db8a8474ba40a1dbc204df3e5be4d94
dilip sahu <sg6422090@gmail.com>
2023-07-08 09:21:41 +0530

    resolver and query variable

--- schema.js
+++ schema.js
@@ -18,8 +18,11 @@ type Author{
 }
 
 type Query{
-    reviews:[Review],
-    games:[Game],
+    reviews:[Review]
+    review(id:ID!):Review
+    games:[Game]
+    game(id:ID!):Game
     authors:[Author]
+    author(id:ID!):Author
 }
 `;
